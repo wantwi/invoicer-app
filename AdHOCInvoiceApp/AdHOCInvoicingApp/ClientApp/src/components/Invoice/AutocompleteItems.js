03e2f797212129634201e5573499d1d8c43ad993
wantwi <wantwi28@gmail.com>
2024-08-07 09:18:52 +0000

    adding item search in progress

--- AdHOCInvoiceApp/AdHOCInvoicingApp/ClientApp/src/components/Invoice/AutocompleteItems.js
+++ AdHOCInvoiceApp/AdHOCInvoicingApp/ClientApp/src/components/Invoice/AutocompleteItems.js
@@ -4,7 +4,9 @@ import { useEffect } from "react";
 import { toast } from "react-toastify";
 import { Input, InputGroup, InputGroupAddon, InputGroupText } from "reactstrap";
 import ItemsSuggestionsList from "./ItemsSuggestionsList";
+import { AsyncTypeahead } from "react-bootstrap-typeahead"
 
+const SEARCH_URI = 'https://api.github.com/search/users';
 const AutocompleteItems = ({
   suggestions,
   formData,
@@ -80,8 +82,48 @@ const AutocompleteItems = ({
     setFilteredSuggestions(suggestions);
   };
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const handleSearch = (query) => {
+    setIsLoading(true);
+
+    fetch(`${SEARCH_URI}?q=${query}+in:login&page=1&per_page=50`)
+      .then((resp) => resp.json())
+      .then(({ items }) => {
+        setOptions(items);
+        setIsLoading(false);
+      });
+  };
+  const filterBy = () => true;
   return (
     <>
+ <AsyncTypeahead
+ size="sm"
+      filterBy={filterBy}
+      id="async-example"
+      isLoading={isLoading}
+      labelKey="login"
+      minLength={3}
+      onSearch={handleSearch}
+      options={options}
+      placeholder="Search for an item by code or name..."
+      renderMenuItemChildren={(option) => (
+        <>
+          <img
+            alt={option.login}
+            src={option.avatar_url}
+            style={{
+              height: '24px',
+              marginRight: '10px',
+              width: '24px',
+            }}
+          />
+          <span>{option.login}</span>
+        </>
+      )}
+    />
+
       <InputGroup className="input-group-alternative" onClick={onFocus}>
         <ItemSugestionInputField
           onChange={onChange}
